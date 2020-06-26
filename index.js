9e63190cea177f5ed418d088f7e62a0be178dc6f
Linux4Life531 <61314680+Linux4Life531@users.noreply.github.com>
2020-06-26 09:28:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,27 @@ app.get('/sketch.js', (req, res) => {
   res.sendFile(__dirname + '/sketch.js');
 });
 
+app.get('/player.js', (req, res) => {
+  res.sendFile(__dirname + '/player.js');
+});
+
+app.get('/Develop.js', (req, res) => {
+  res.sendFile(__dirname + '/Develop.js');
+});
+
 io.on('connection', (socket) => {
   console.log('A user connected');  
   socket.on('disconnect', () => {
     console.log('A User disconnected');
   });
+  socket.on('pos', (pos) => {
+    console.log('Position: X: ' + pos.x + ' Y: ' + pos.y);
+    socket.on('pos', (msg) => {
+      socket.broadcast.emit('pos', msg);
+    });
+  });
 });
 
 http.listen(3000, () => {
-  console.log('Listening on https://Laserio--jamesashwood.repl.co');
+  console.log('Listening on https://WildWestIO--iogame.repl.co');
 });
